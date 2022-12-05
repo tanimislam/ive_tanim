3eca867de754ec00a9a816f08760ae6905e6770a
Tanim Islam <islam5@llnl.gov>
2022-12-04 16:43:48 -0800

    added in more BINARY MESSAGE functionalities.

--- iv_tanim/cli/myrst2html.py
+++ iv_tanim/cli/myrst2html.py
@@ -2,34 +2,7 @@ import os, sys
 from iv_tanim.core import rst2html
 from argparse import ArgumentParser
 
-# class MyHTMLTranslator(HTMLTranslator):
-#     mathjax_script = '<script type="text/javascript" src="{}"></script>\n'
-#     mathjax_url = 'https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML'
-
-#     def visit_math(self, node, math_env=''):
-#         if self.math_output != 'mathjax':
-#             super().visit_math(node, math_env)
-#             return
-
-#         if self.math_output == 'mathjax' and not self.math_header:
-#             if self.math_output_options:
-#                 self.mathjax_url = self.math_output_options[0]
-#             self.math_header = [self.mathjax_script.format(self.mathjax_url)]
-
-#         math_code = node.astext().translate(unichar2tex.uni2tex_table)
-#         math_code = self.encode(math_code)
-#         # Don't wrap the mathematics with div or span
-#         if math_env:
-#             self.body.append('\[{}\]\n'.format(math_code))
-#         else:
-#             self.body.append('\({}\)'.format(math_code))
-#         raise nodes.SkipNode
-
 def _main( ):
-    #htmlwriter = Writer()
-    # htmlwriter.translator_class = MyHTMLTranslator
-    #publish_cmdline(writer=htmlwriter)
-
     parser = ArgumentParser( )
     parser.add_argument('-i', '--input', dest='inputfile', type = str, action = 'store', required = True,
                         help = 'Name of the input RST file. Required argument.' )
--- iv_tanim/core/rst2html.py
+++ iv_tanim/core/rst2html.py
@@ -1,10 +1,23 @@
 #
 ## docutils stuff, which I am putting in one spot now because I don't understand what's going on
 # from docutils.examples import html_parts
-import os, sys, logging, copy
+import os, sys, logging, magic, validators, uuid
 from bs4 import BeautifulSoup
 from docutils import core, nodes
 from docutils.writers.html4css1 import Writer, HTMLTranslator
+#
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+from email.mime.audio import MIMEAudio
+from email.mime.image import MIMEImage
+from email.utils import formataddr, parseaddr
+from email import message_from_bytes
+
+##
+## read  email message into mimemultipart from file in bytes: msg = message_from_bytes( open( 'msg_file.msg', 'rb' ).read( ) )
+## write email message as bytes: with open( 'msg_file.msg', 'wb' ) as openfile: openfile.write( msg.as_bytes( ) )
+##
 
 class MyHTMLTranslator( HTMLTranslator ):
     """
@@ -124,3 +137,153 @@ def convert_string_RST( myString, use_mathjax = False, outputfilename = None ):
     #
     html = _fix_title_elem( html )
     return html.prettify( )
+
+def create_rfc2047_email( email_fullname_dict ):
+    """
+    Given a :py:class:`dict` containing email address and (optionally) full name, returns the `RFC 2047`_ fully qualified email address.
+    
+    :param dict email_fullname_dict: the :py:class:`dict` that *should* contain the email and optionally the fully qualified name. Email address is in the ``email`` key, and optional full name is in the ``full name`` key.
+    :returns: an `RFC 2047`_ fully qualified email address under the following conditions: 1) if there is an email address; and 2) if there is an email address *AND* a fully qualified name. Otherwise returns ``None``.
+    :rtype: str
+
+    .. _`RFC 2047`: https://tools.ietf.org/html/rfc2047.html
+    """
+    input_tuple = [ '', '' ]
+    if 'email' in email_fullname_dict and len( email_fullname_dict[ 'email' ].strip( ) ) > 0:
+        input_tuple[1] = email_fullname_dict[ 'email' ]
+    if 'full name' in email_fullname_dict and len( email_fullname_dict[ 'full name' ].strip( ) ) > 0:
+        input_tuple[0] = email_fullname_dict[ 'full name' ]
+    #
+    if input_tuple[0] == '' and input_tuple[1] == '':
+        logging.error("ERROR, NO VALID EMAIL AND FULL NAME FORM %s." % email_fullname_dict )
+        return None
+    if input_tuple[1] == '':
+        logging.error("ERROR, NO VALID EMAIL FROM %s" % email_fullname_dict )
+        return None
+    #
+    try:
+        return formataddr( tuple( input_tuple ) )
+    except Exception as e:
+        logging.error( "Problem with trying to format as email this tuple: %s. Exception = %s." % (
+            input_tuple, str( e ) ) )
+        return None
+
+def parse_rfc2047_email( candidate_rfc2047_email ):
+    """
+    Uses :py:meth:`parseaddr <email.utils.parseaddr>` to create a :py:class:`dict` of candidate email dictionary (keys are ``email`` and optionally ``full name``).
+
+    :param str candidate_rfc2047_email: the input fully qualified email address.
+    :returns: a :py:class:`dict` of candidate email dictionary *only* if there is a valid email address. Otherwise returns ``None``.
+    :rtype: dict
+    """
+    output_tuple = parseaddr( candidate_rfc2047_email )
+    if output_tuple[1].strip( ) == '':
+        logging.error("Error, candidate input email = %s does NOT have a valid email address." % candidate_rfc2047_email )
+        return None
+    return { 'email' : output_tuple[1].strip( ), 'full name' : output_tuple[0].strip( ) }
+
+def cid_out_mimeMultiMessage( msg, mainHTML ):
+    """
+    Goes through the HTML email message, and creates a CID-enabled email with additional image attachments inside.
+
+    :param msg: the :py:class:`MIMEMultiPart <email.mime.multipart.MIMEMultiPart>` message object that we will modify.
+    :param str mainHTML: the HTML message that we will parse through for images on disk rather than as external URLs.
+    :returns: the :py:class:`dict` of ``cid`` to image file name.
+    :rtype: dict
+    """
+    htmldoc = BeautifulSoup( mainHTML, 'lxml' )
+    #
+    ## find all img elems that have a "src" in them and that ARE NOT an URL
+    valid_imgs = list(filter(lambda elem: 'src' in elem.attrs and not validators.url( elem['src'] ), htmldoc.find_all('img')))
+    if len( valid_imgs ) == 0: # don't modify document
+        msg.attach( MIMEText( mainHTML, 'html', 'utf-8' ) )
+        return dict()
+    #
+    ## now perform a replacement and create attachments
+    ## follow https://stackoverflow.com/a/20485764
+    cid_replacement = dict()
+    for img_elem in valid_imgs:
+        cid = str( uuid.uuid4( ) )
+        fname = img_elem['src']
+        cid_replacement[ cid ] = fname
+        img_elem[ 'src' ] = "cid:%s" % cid
+        img_elem[ 'alt' ] = fname
+    #
+    ## now attach the *changed* message
+    msg.attach( MIMEText( htmldoc.prettify( ), 'html', 'utf-8' ) )
+    for cid in cid_replacement:
+        msg_image = MIMEImage(
+            open( cid_replacement[ cid ], 'rb' ).read( ),
+            name = os.path.basename( cid_replacement[ cid ] ) )
+        msg_image.add_header( 'Content-ID', '<%s>' % cid )
+        msg.attach( msg_image )
+    return cid_replacement
+
+def create_collective_email_full(
+    mainHTML, subject, fromEmail, to_emails, cc_emails = [ ], bcc_emails = [ ], attachments = [ ] ):
+    """
+    Creates a :py:class:`MIMEMultiPart <email.mime.multipart.MIMEMultiPart>` email that *also* contains the following
+
+    * the body of the message as an HTML document.
+
+    * sender.
+
+    * ``TO`` recipients.
+
+    * ``CC`` recipients.
+
+    * ``BCC`` recipients.
+
+    * explicitly included attachments.
+
+    * soft-conventioned ``cid`` of embedded and inlined images (PNG_, JPEG_, TIFF_).
+
+    Go easy on me, it's my first day!
+
+    :param str mainHTML: the email body as an HTML :py:class:`string <str>` document.
+    :param str subject: the email subject.
+    :param str fromEmail: the :py:class:`dict` of email and optional full name of sender.
+    :param set to_emails: the :py:class:`list` of :py:class`dict` of ``TO`` recipients. Each recipient
+    :param set cc_emails: the `RFC 2047`_ :py:class:`set` of ``CC`` recipients.
+    :param set bcc_emails: the `RFC 2047`_ :py:class:`set` of ``BCC`` recipients.
+    :param list attachments: the collection of attachments to send out.
+    :returns: the :py:class:`MIMEMultiPart <email.mime.multipart.MIMEMultiPart>` of this message, with soft-conventioned ``cid`` of images included 
+    """
+    #
+    ## get the RFC 2047 sender stuff
+    msg = MIMEMultipart( )
+    fq_fromEmail = create_rfc2047_email( fromEmail )
+    assert( fq_fromEmail is not None )        
+    msg[ 'From' ] = fq_fromEmail
+    msg[ 'Subject' ] = subject
+    msg[ 'To' ] = ', '.join( sorted(set(filter(None, map(create_rfc2047_email,  to_emails))))).strip( )
+    msg[ 'Cc' ] = ', '.join( sorted(set(filter(None, map(create_rfc2047_email,  cc_emails))))).strip( )
+    msg[ 'Bcc'] = ', '.join( sorted(set(filter(None, map(create_rfc2047_email, bcc_emails))))).strip( )
+    logging.info( 'from_email: %s' % msg[ 'From' ] )
+    logging.info( 'to_emails: %s.' % msg['To'] )
+    logging.info( 'cc_emails: %s.' % msg['Cc'] )
+    logging.info('bcc_emails: %s.' % msg['Bcc'])
+    #msg.attach( MIMEText( mainHTML, 'html', 'utf-8' ) )
+    cid_out_mimeMultiMessage( msg, mainHTML )
+    #
+    ##
+    if len( attachments ) == 0: return msg
+    #
+    for attach in attachments:
+        name = attach[ 'name' ]
+        mimetype = attach[ 'mimetype' ]
+        filepath = attach[ 'filepath' ]
+        mainType, subtype = mimetype.split('/')[:2]
+        if mainType == 'application':
+            att = MIMEApplication( open( filepath, 'rb' ).read( ), _subtype = subtype )
+        elif mainType == 'text':
+            att = MIMEText( open( filepath, 'r' ).read( ), _subtype = subtype )
+        elif mainType == 'image':
+            att = MIMEImage( open( filepath, 'rb' ).read( ), _subtype = subtype )
+        elif mainType == 'audio':
+            att = MIMEAudio( open( filepath, 'rb' ).read( ), _subtype = subtype )
+        else:
+            att = MIMEApplication( open( filepath, 'rb' ).read( ) )
+        att.add_header( 'content-disposition', 'attachment', filename = name )
+        msg.attach( att )
+    return msg
